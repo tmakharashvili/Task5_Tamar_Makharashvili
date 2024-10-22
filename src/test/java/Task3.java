f095b148814aeb97992860311e9a138a4d5b81cb
tamar makharashvili <tamarmakharashvili@credo.ge>
2024-10-22 22:58:08 +0400

    Task_3_Tamuna

---
+++ src/test/java/Task3.java
@@ -0,0 +1,9 @@
+public class Task3 {
+    public static void main(String[] args) {
+        double area = calculateArea(5.5, 4.0);
+        System.out.println(area);
+    }
+    public static double calculateArea(double length, double width){
+        return length*width;
+    }
+}
